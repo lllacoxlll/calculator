d7c6e1e8c13c1374033aeb496ca4286d7eba0254
lllacoxlll <65261676+lllacoxlll@users.noreply.github.com>
2025-06-01 21:57:13 -0500

    Create calculator buttons and display and add styling

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-let containerDiv = document.querySelector('#container')
-containerDiv.style.border = '2px solid red'
+function add(num1, num2) {
+  return num1 + num2
+}
+
+function subtract(num1, num2) {
+  return num1 - num2
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+function divide(num1, num2) {
+  return num1 / num2
+}
+
+// let num1 = prompt('First number:')
+// let operator = prompt('Operator')
+// let num2 = prompt('Second number:')
+
+function operate(num1, operator, num2) {
+  if (operator === '+') alert(add(num1, num2))
+  if (operator === '-') alert(subtract(num1, num2))
+  if (operator === '*') alert(multiply(num1, num2))
+  if (operator === '/') alert(divide(num1, num2))
+}
