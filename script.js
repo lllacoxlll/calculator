bcd6bd0f534df9935e0d3faacb09aab9f484378a
lllacoxlll <65261676+lllacoxlll@users.noreply.github.com>
2025-06-05 15:25:26 -0500

    Add js functionality for calculator buttons to populate display and call operate function

--- script.js
+++ script.js
@@ -14,13 +14,33 @@ function divide(num1, num2) {
   return num1 / num2
 }
 
-// let num1 = prompt('First number:')
-// let operator = prompt('Operator')
-// let num2 = prompt('Second number:')
-
 function operate(num1, operator, num2) {
-  if (operator === '+') alert(add(num1, num2))
-  if (operator === '-') alert(subtract(num1, num2))
-  if (operator === '*') alert(multiply(num1, num2))
-  if (operator === '/') alert(divide(num1, num2))
+  if (operator === '+') return add(num1, num2)
+  if (operator === '-') return subtract(num1, num2)
+  if (operator === '*') return multiply(num1, num2)
+  if (operator === '/') return divide(num1, num2)
 }
+
+let childBtns = document.querySelectorAll('.childButton')
+let display = document.querySelector('#display')
+let operateArray = []
+
+childBtns.forEach((childBtn) =>
+  childBtn.addEventListener('click', function (e) {
+    let buttonVal = e.target.textContent
+    if (buttonVal === 'AC') {
+      display.textContent = ''
+      operateArray.length = 0
+    } else if (buttonVal === '=') {
+      let toSplit = display.textContent
+      let operator = toSplit.match(/[-+/*]/)
+      let operateArray = toSplit.split(operator[0])
+      let res = operate(parseFloat(operateArray[0]), operator[0], parseFloat(operateArray[1]))
+      display.textContent = res
+      operateArray.length = 0
+      operateArray.push(display.textContent)
+    } else {
+      display.textContent += buttonVal
+    }
+  })
+)
